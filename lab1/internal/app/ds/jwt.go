d8b1762538bddac51ee1c093230be78e2ca116e1
Qrp34ch <168339978+Qrp34ch@users.noreply.github.com>
2025-10-15 00:16:29 +0300

    add_auth_and_swagger

---
+++ lab1/internal/app/ds/jwt.go
@@ -0,0 +1,13 @@
+package ds
+
+import (
+	"github.com/golang-jwt/jwt"
+	"github.com/google/uuid"
+)
+
+type JWTClaims struct {
+	jwt.StandardClaims
+	UserID   uint      `json:"user_id"`
+	UserUUID uuid.UUID `json:"user_uuid"`
+	Scopes   []string  `json:"scopes"`
+}
